983a3681a488da4c2001f2754c49f0929fba761a
Altelulex <a.trop@gmx.de>
2026-03-07 19:10:55 +0100

    push dev tag

--- Ara_Broker_Reputations.lua
+++ Ara_Broker_Reputations.lua
@@ -17,7 +17,7 @@ local updateBeforeBlizzard, watchedFaction, watchedIndex, focusedButton, barFact
 local sliderValue, hasSlider, c, nbEntries = 0, false, {}, 0
 local prevSkin, tiptacBG, tiptacGradient
 local defaultTexture = "Interface\\TargetingFrame\\UI-StatusBar"
-local addondevversion = "r98"
+local addondevversion = "r99"
 local defaultConfig = {
     scale = 1.1,
     blockDisplay = "text",
